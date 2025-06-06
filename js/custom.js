78521e8bdf3b7cb55e8b1e79b4447ce8861fc757
jsalazarmjv <jsalazarmjv@gmail.com>
2025-06-05 23:29:22 -0700

    Gallery Update

--- js/custom.js
+++ js/custom.js
@@ -12,11 +12,11 @@ let maxImages = 1;
 
 // Album data with folder keys, image count, and prefix for images
 const albums = {
-  "GalleryBDays": {count: 31, prefix: "Bday"},
-  "GalleryConcert": {count: 26, prefix: "Concerts"},
-  "GalleryCorp": {count: 18, prefix: "Corp"},
+  "GalleryBDays": {count: 30, prefix: "Bday"},
+  "GalleryConcert": {count: 25, prefix: "Concerts"},
+  "GalleryCorp": {count: 17, prefix: "Corp"},
   "GalleryFamily": {count: 18, prefix: "Family"},
-  "GalleryGrad": {count: 19, prefix: "Grads"},
+  "GalleryGrad": {count: 18, prefix: "Grads"},
   "GalleryIndoor": {count: 31, prefix: "Indoor"},
   "GalleryOutdoor": {count: 37, prefix: "Outdoor"},
   "GalleryRealState": {count: 10, prefix: "RealState"},
